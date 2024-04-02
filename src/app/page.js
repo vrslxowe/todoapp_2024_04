1bff7ccf6dae0f131dc9d429b31265a4bf4c0ce1
vrslxowe <vrslxowe@gmail.com>
2024-04-02 16:06:09 +0900

    MUI Image List 컴포넌트

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,67 @@
 
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { Button, AppBar, Toolbar, Tabs, Tab } from '@mui/material';
+import { Button, AppBar, Toolbar, ImageList, ImageListItem } from '@mui/material';
 import theme from './theme';
 import { FaBars } from 'react-icons/fa';
 import Link from 'next/link';
 
 export default function App() {
-  const [tabCurrentIndex, setTabCurrentIndex] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const itemData = [
+    {
+      img: 'https://blog.kakaocdn.net/dn/bmqXqj/btsFYN6qP5h/kPaI2XVZmxnkw8lWkeNIl0/img.jpg',
+      title: 'Breakfast',
+    },
+    {
+      img: 'https://blog.kakaocdn.net/dn/b1jClb/btsFXY8xE1I/0ZYEE6bO5GscllLtZT3uh1/img.jpg',
+      title: 'Coffee',
+    },
+    {
+      img: 'https://blog.kakaocdn.net/dn/b1Sn6l/btsGc3IJaP9/bAYukRd89v6w8wb9qbw7GK/img.jpg',
+      title: 'Hats',
+    },
+    {
+      img: 'https://blog.kakaocdn.net/dn/bIaHUa/btsGbBsU2vs/5MJjIJDRJKCHiqNzpA8PZK/img.jpg',
+      title: 'Burger',
+    },
+    {
+      img: 'https://blog.kakaocdn.net/dn/ch5xpk/btsGcHTqzJm/w7UD46JENdnghIbQk5f5dk/img.jpg',
+      title: 'Camera',
+    },
+    {
+      img: 'https://blog.kakaocdn.net/dn/bjobPW/btsGbS8Zsqs/REImSzTlkU4Lw1CCtn3HqK/img.jpg',
+      title: 'Honey',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+      title: 'Basketball',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+      title: 'Fern',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+      title: 'Mushrooms',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+      title: 'Tomato basil',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+      title: 'Sea star',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+      title: 'Bike',
+    },
+  ];
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -33,14 +86,18 @@ export default function App() {
           section
         </section>
       </ThemeProvider>
-      <Tabs value={tabCurrentIndex} onChange={(_, newValue) => setTabCurrentIndex(newValue)}>
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
-      </Tabs>
-      {tabCurrentIndex == 0 && <div>내용1</div>}
-      {tabCurrentIndex == 1 && <div>내용2</div>}
-      {tabCurrentIndex == 2 && <div>내용3</div>}
+      <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </>
   );
 }
